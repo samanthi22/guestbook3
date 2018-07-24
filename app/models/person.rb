e41f057ce345d470540ad2a3daadbdff6a7c2cbf
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2018-07-24 13:50:09 +0000

    validates if description

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,9 @@ class Person < ApplicationRecord
     validates_presence_of :name
     validates_presence_of :secret, in: 6..24
     validates_inclusion_of :country, in: ['Canada', 'Mexico', 'UK', 'USA'],  message: "must be one of Canada, Mexico, UK, USA"
+    validates_pressence_of :description, if: :require_description_presence?
+    
+    def require_description_presence?
+        self.can_send_email
+    end
 end
