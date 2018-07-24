09e2f745063510e6ad264069f7dc3862a5aa1e10
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2018-07-24 14:52:32 +0000

    Added helper methods for form

---
+++ app/helpers/tidy_form_builder.rb
@@ -0,0 +1,31 @@
+class TidyFormBuilder < ActionView::Helpers::FormBuilder
+   def country_select(method, options={}, html_options={})
+       select(method, [["Canada", "Canada"],
+       ["Mexico", "Mexico"],
+       ["United Kingdom", "UK"],
+       ["United States of America", "USA"]],
+       options, html_options)
+   end
+   
+   def text_field(method, option={})
+       label = label_for(method, options) + super(method, options)
+   end
+   
+    def time_select(method, options={}, html_options = {})
+       label_for(method, options) + super(method, options, html_options)
+   end
+   
+   def select(method, choices, options={}, html_options = {})
+       label_for(method, options) + super(method, choices, options, html_options)
+   end
+   
+   def check_box(method, options={}, checked_value = "1", unchecked_value = "0")
+       label_for(method, options) + super(method, options, checked_value, unchecked_value)
+   end
+   
+   private
+   def label_for(method, options={})
+       label(options.delete(:label) || method).safe_concat("<br />")
+   end
+    
+end
