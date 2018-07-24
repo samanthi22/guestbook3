4d75075477342aea4865fc08769add1fad9f9ca9
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2018-07-24 16:08:02 +0000

    Trying to use DRY on <p> and <div> tags in form.html.erb

--- app/helpers/tidy_form_builder.rb
+++ app/helpers/tidy_form_builder.rb
@@ -1,4 +1,11 @@
 class TidyFormBuilder < ActionView::Helpers::FormBuilder
+    def country_select(method, options={}, html_options={})
+       select(method, [["Canada", "Canada"],
+       ["Mexico", "Mexico"],
+       ["United Kingdom", "UK"],
+       ["United States of America", "USA"]],
+       options, html_options)
+   end
    
    def text_field(method, option={})
        wrap_field(label = label_for(method, options) + super(method, options), option)
@@ -12,13 +19,6 @@ class TidyFormBuilder < ActionView::Helpers::FormBuilder
        wrap_field(label_for(method, options) + super(method, choices, options, html_options))
    end
    
-   def country_select(method, options={}, html_options={})
-       select(method, [["Canada", "Canada"],
-       ["Mexico", "Mexico"],
-       ["United Kingdom", "UK"],
-       ["United States of America", "USA"]],
-       options, html_options)
-   end
    
    def check_box(method, options={}, checked_value = "1", unchecked_value = "0")
        label_for(method, options) + super(method, options, checked_value, unchecked_value)
