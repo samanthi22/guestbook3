3a3e2ad229b7c8f83b0beeff543a48c0a6234f16
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2018-07-24 13:40:12 +0000

    Added validation to the model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
 class Person < ApplicationRecord
     validates_presence_of :name
+    validates_presence_of :secret, in: 6..24
+    validates_inclusion_of :country, in: ['Canada', 'Mexico', 'UK', 'USA'],  message: "must be one of Canada, Mexico, UK, USA"
 end
