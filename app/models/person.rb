e6c71d8c15b8d77d509b4034717da2e3102b8b78
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2018-07-24 13:51:49 +0000

    Fixed typo

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ApplicationRecord
     validates_presence_of :name
     validates_presence_of :secret, in: 6..24
     validates_inclusion_of :country, in: ['Canada', 'Mexico', 'UK', 'USA'],  message: "must be one of Canada, Mexico, UK, USA"
-    validates_pressence_of :description, if: :require_description_presence?
+    validates_presence_of :description, if: :require_description_presence?
     
     def require_description_presence?
         self.can_send_email
